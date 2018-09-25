a55bd40023ad034e16b9a3bbfc38ac8ec5bfc6d9
Dominika Szukała <dominika.szukala@gmail.com>
2018-09-25 13:26:12 +0200

    Cute color changing line added

--- js/app.js
+++ js/app.js
@@ -1,29 +1,36 @@
 const canvas = document.querySelector('#draw');
 const ctx = canvas.getContext('2d');
-canvas.width = canvas.scrollWidth;
-canvas.height = canvas.scrollHeight;
+canvas.width = 800;
+canvas.height = 800;
 
-ctx.strokeStyle = '#BADA55'; // start colour
 ctx.lineJoin = 'round';  // joining line with round finish
 ctx.lineCap = 'round'; // finish point with round corners
+ctx.lineWidth = 11;
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
 
 function draw(e) {
     if (!isDrawing) return;
-    console.log(e);
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`
     ctx.beginPath();
+//    start form the last point
     ctx.moveTo(lastX, lastY);
+//    go to the point
     ctx.lineTo(e.offsetX, e.offsetY);
     ctx.stroke();
-    lastX = e.offsetX;
-    lastY = e.offsetY;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+    hue++
 }
 
+canvas.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+    ctx.lineTo(e.offsetX, e.offsetY);
+});
 
 canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mousedown', () => isDrawing = true);
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
